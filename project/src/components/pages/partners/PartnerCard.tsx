ae5571ddc7b89944a82854e8e51249b9f45d317c
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-04-11 15:27:05 +0900

    🎨 Style : 메인 파트너 카드 UI 수정

--- project/src/components/pages/partners/PartnerCard.tsx
+++ project/src/components/pages/partners/PartnerCard.tsx
@@ -1,13 +1,11 @@
 import { getCurrentUserIsSignIn, getCurrentUserUID } from "@/service/auth";
 import { getIsAlreadyPartnerRelationship, postPartnerRelationship } from "@/service/partner-relationship";
 import { UserInformationType } from "@/types/user";
-import { Button, Grid2, styled, Typography } from "@mui/material";
+import { Button, Grid2, Stack, styled, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { enqueueSnackbar } from "notistack";
 
 const PartnerCard = ({ info }: { info: UserInformationType }) => {
-
-  
   const router = useRouter();
   // 파트너 신청 함수
   async function handleAddPartner() {
@@ -47,25 +45,87 @@ const PartnerCard = ({ info }: { info: UserInformationType }) => {
     enqueueSnackbar("파트너 신청 성공", { variant: "success" });
   }
 
+  // 이메일 복사 함수
+  const copyEmailToClipboard = () => {
+    if (info.email) {
+      navigator.clipboard
+        .writeText(info.email)
+
+        .then(() => {
+          enqueueSnackbar("이메일이 복사되었습니다.", { variant: "success" });
+        })
+
+        .catch(() => {
+          enqueueSnackbar("이메일 복사에 실패했습니다.", { variant: "error" });
+        });
+    }
+  };
+
   return (
-    <Container size={6}>
-      <Typography variant="h6" align="center" sx={{ width: "100%" }}>
-        {info.app_name}
-      </Typography>
-      <Button onClick={handleAddPartner} variant="contained" color="primary" fullWidth sx={{ color: "white" }}>
-        파트너 신청
-      </Button>
-    </Container>
+    <Grid2 size={6}>
+      <Container>
+        <InfoWrapper>
+          <AppNameText variant="caption">{info.app_name}</AppNameText>
+          <EmailText variant="body2">{info.email}</EmailText>
+        </InfoWrapper>
+        <ButtonWrapper>
+          <EmailCopyButton onClick={copyEmailToClipboard} variant="outlined">
+            이메일 복사
+          </EmailCopyButton>
+          <AddPartnerButton onClick={handleAddPartner} variant="contained">
+            파트너 신청
+          </AddPartnerButton>
+        </ButtonWrapper>
+      </Container>
+    </Grid2>
   );
 };
 
 export default PartnerCard;
 
-const Container = styled(Grid2)`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  border: 1px solid ${({ theme }) => theme.palette.primary.dark};
+const Container = styled(Stack)`
+  overflow: hidden;
+  row-gap: 8px;
+  border: 1px solid ${({ theme }) => theme.palette.primary.main};
   border-radius: 8px;
-  padding: 16px;
+`;
+
+const InfoWrapper = styled(Stack)`
+  padding: 8px;
+  row-gap: 4px;
+  text-align: center;
+`;
+
+const AppNameText = styled(Typography)`
+  // 한줄 이상 넘어가면 ... 표시
+  display: -webkit-box;
+  -webkit-line-clamp: 1; // 3줄 이상이면 ellipsis 적용
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+`;
+
+const EmailText = styled(AppNameText)``;
+
+const ButtonWrapper = styled(Stack)`
+  flex-direction: row;
+  column-gap: 4px;
+  border-top: 1px solid ${({ theme }) => theme.palette.primary.main};
+`;
+
+const EmailCopyButton = styled(Button)`
+  height: 32px;
+  color: ${({ theme }) => theme.palette.primary.main};
+  padding: 0px;
+  font-size: 12px;
+  flex: 1;
+  border: none;
+`;
+const AddPartnerButton = styled(EmailCopyButton)`
+  height: 32px;
+  color: ${({ theme }) => theme.palette.text.white};
+  padding: 0px;
+  border-radius: 0px;
 `;
